d2cfa92d7fa8a3de57af1663b4acc5096d153f69
Nicolas Vallejo <vallejofernandonicolas@gmail.com>
2023-11-22 20:38:53 -0300

    changes in dependencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import * as math from "mathjs";
+import "./App.css";
 
 function App() {
+  const [pantalla, setPantalla] = useState("0");
+  const [ultimaOperacion, setUltimaOperacion] = useState(null);
+  const [resetPantalla, setResetPantalla] = useState(false);
+
+  const agregarDigito = (digito) => {
+    if (pantalla === "0" && digito === "0") {
+      return;
+    }
+
+    if (resetPantalla) {
+      setPantalla(digito);
+      setResetPantalla(false);
+    } else {
+      setPantalla((prevPantalla) =>
+        prevPantalla === "0" ? digito : prevPantalla + digito
+      );
+    }
+  };
+  const agregarOperador = (operador) => {
+    if (ultimaOperacion === "=") {
+      setUltimaOperacion(null);
+    }
+
+    const lastChar = pantalla.charAt(pantalla.length - 1);
+    const isOperator = ["+", "-", "*", "/"].includes(lastChar);
+
+    if (!isOperator || (isOperator && operador === "-" && lastChar !== "-")) {
+      setPantalla((prevPantalla) => prevPantalla + operador);
+      setUltimaOperacion(operador);
+      setResetPantalla(false);
+    } else if (isOperator && operador !== "-" && lastChar !== "-") {
+      // Remove the previous operator and replace it with the new operator
+      setPantalla((prevPantalla) => prevPantalla.slice(0, -1) + operador);
+      setUltimaOperacion(operador);
+      setResetPantalla(false);
+    }
+  };
+
+  const limpiarPantalla = () => {
+    setPantalla("0");
+    setUltimaOperacion(null);
+    setResetPantalla(false);
+  };
+
+  const calcular = () => {
+    let expresion = pantalla;
+    let resultado;
+
+    expresion = expresion.replace(/--/g, "+");
+    expresion = expresion.replace(/-/g, "+-");
+
+    try {
+      resultado = math.evaluate(expresion);
+    } catch (error) {
+      resultado = "Error";
+    }
+    setPantalla(resultado.toString());
+    setUltimaOperacion("=");
+    setResetPantalla(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculadora">
+        <div id="display">{pantalla}</div>
+        <div className="numeros">
+          <button id="zero" onClick={() => agregarDigito("0")}>
+            0
+          </button>
+          <button id="one" onClick={() => agregarDigito("1")}>
+            1
+          </button>
+          <button id="two" onClick={() => agregarDigito("2")}>
+            2
+          </button>
+          <button id="three" onClick={() => agregarDigito("3")}>
+            3
+          </button>
+          <button id="four" onClick={() => agregarDigito("4")}>
+            4
+          </button>
+          <button id="five" onClick={() => agregarDigito("5")}>
+            5
+          </button>
+          <button id="six" onClick={() => agregarDigito("6")}>
+            6
+          </button>
+          <button id="seven" onClick={() => agregarDigito("7")}>
+            7
+          </button>
+          <button id="eight" onClick={() => agregarDigito("8")}>
+            8
+          </button>
+          <button id="nine" onClick={() => agregarDigito("9")}>
+            9
+          </button>
+          <button id="decimal" onClick={() => agregarDigito(".")}>
+            .
+          </button>
+          <button id="clear" onClick={limpiarPantalla}>
+            Limpiar
+          </button>
+        </div>
+
+        <div className="operadores">
+          <button id="add" onClick={() => agregarOperador("+")}>
+            +
+          </button>
+          <button id="subtract" onClick={() => agregarOperador("-")}>
+            -
+          </button>
+          <button id="multiply" onClick={() => agregarOperador("*")}>
+            *
+          </button>
+          <button id="divide" onClick={() => agregarOperador("/")}>
+            /
+          </button>
+          <button id="equals" onClick={calcular}>
+            =
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
